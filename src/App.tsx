d608c473acda46a19a95356bdbf3ef7999394464
sinsky <60662802+sinsky@users.noreply.github.com>
2022-10-14 10:07:23 +0900

    layout add navigations

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,20 @@ import { useState } from "react";
 import { Route, RouterProvider, createHashRouter, createRoutesFromElements } from "react-router-dom";
 import { Layout } from "./Layout";
 
+export type PathProps = {
+  path: string;
+  title: string;
+};
+
+const paths = [
+  { path: "/", title: "Home" },
+  { path: "/test", title: "Test" },
+];
+
 const App = () => {
   const router = createHashRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Layout paths={paths} />}>
         <Route index element={<h1 className="text-slate-800">Home</h1>} />
         <Route path="test" element={<h1 className="text-green-800">Test</h1>} />
       </Route>
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import { PathProps } from "./App";
 
-export const Layout = () => {
+export const Layout = ({ paths }: { paths: PathProps[] }) => {
+  const navItems = paths.map((path) => (
+    <NavLink
+      key={path.path}
+      to={path.path}
+      className={({ isActive }) =>
+        (isActive
+          ? "cursor-pointer pointer-events-none text-white border-green-400 bg-green-600"
+          : "text-blue-500 border-blue-600 hover:text-blue-600 hover:border-blue-700") + " border"
+      }
+      end
+    >
+      <div className="px-4 py-2">{path.title}</div>
+    </NavLink>
+  ));
   return (
     <>
       <div className="flex flex-col my-4 border-b">
         <div className="font-bold text-blue-800 font-xl">Nav</div>
-        <div className="flex my-4 border-b"></div>
+        <div className="flex mt-4">{navItems}</div>
       </div>
       <Outlet />
     </>
