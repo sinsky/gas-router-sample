abb3912049eb2d8a0d9edf96ce1daf2e4c785ab8
sinsky <60662802+sinsky@users.noreply.github.com>
2022-10-14 09:46:08 +0900

    add vite plugin and setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), viteSingleFile()],
+  // 正直いらない, buildファイルの場所を変えたいならoutDirくらい
+  base: "./",
+  build: {
+    outDir: "./dist/",
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name][extname]",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
