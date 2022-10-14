39826772a488aa697c13a5184a35404a3c99a739
sinsky <60662802+sinsky@users.noreply.github.com>
2022-10-14 09:47:09 +0900

    re fomarted

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <div></div>
-  )
+  return <div></div>;
 }
 
-export default App
+export default App;
